04f987a62eed4f889eb837dc8152ac2675ff6bba
o-murphy <thehelixpg@gmail.com>
2022-06-24 22:31:24 +0300

    update

--- bmath/__init__.py
+++ bmath/__init__.py
@@ -1,2 +1,5 @@
 from .vector import Vector
 from .unit import Weight, Velocity, Temperature
+from .unit import Pressure, Energy, Distance, Angular
+from .unit import WeightConvertor, VelocityConvertor, TemperatureConvertor
+from .unit import PressureConvertor, EnergyConvertor, DistanceConvertor, AngularConvertor
--- bmath/unit/__init__.py
+++ bmath/unit/__init__.py
@@ -1,3 +1,7 @@
-from .weight import Weight
-from .velocity import Velocity
-from .temperature import Temperature
+from .weight import Weight, WeightConvertor
+from .velocity import Velocity, VelocityConvertor
+from .temperature import Temperature, TemperatureConvertor
+from .pressure import Pressure, PressureConvertor
+from .energy import Energy, EnergyConvertor
+from .distance import Distance, DistanceConvertor
+from .angular import Angular, AngularConvertor
