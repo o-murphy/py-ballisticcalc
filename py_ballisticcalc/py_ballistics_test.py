9679355c19f239da9c8733ec20428ea99c27744e
Murphy <thehelixpg@gmail.com>
2022-06-28 16:44:13 +0300

    updated py_ballistics_test.py

---
+++ py_ballisticcalc/py_ballistics_test.py
@@ -0,0 +1,14 @@
+"""
+TODO: Unittests
+"""
+
+import math
+import unittest
+
+from py_ballisticcalc import *
+from py_ballisticcalc.bmath import unit
+
+
+if __name__ == '__main__':
+    unittest.main()
+
