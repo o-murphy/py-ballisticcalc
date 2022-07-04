5dd744e7819918b3f3b647cd4fa3336ebb7e449b
Murphy <thehelixpg@gmail.com>
2022-07-04 13:08:22 +0300

    init multiple_ballistic_coefficient.py

---
+++ py_ballisticcalc/extended/multiple_ballistic_coefficient.py
@@ -0,0 +1,12 @@
+class MultipleBallisticCoefficient(object):
+    """ MultipleBallisticCoefficient object contains multiple bc with specified data """
+
+    def __init__(self):
+        pass
+
+
+class MultiBC2CustomDrag(object):
+    """ Calculates custom drag table for multiple ballistic coefficient """
+
+    def __init__(self):
+        pass
