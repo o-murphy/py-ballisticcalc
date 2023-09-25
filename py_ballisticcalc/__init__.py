07e7e2570f1eca604e5d59162399fa6d2c2b8570
Murphy <thehelixpg@gmail.com>
2023-09-25 19:11:05 +0300

    README.md updated

--- py_ballisticcalc/__init__.py
+++ py_ballisticcalc/__init__.py
@@ -4,8 +4,4 @@ __copyright__ = ("",)
 __credits__ = ["O-Murphy"]
 __version__ = "1.1.0a2"
 
-__all__ = ('settings',
-           'interface',
-           'unit',
-           'drag_tables',
-           'drag_model')
+__all__ = ('interface', )
