b33ab9e49594bfc7c11a63be74d0e8d10f7890ed
Murphy <thehelixpg@gmail.com>
2023-10-18 14:04:18 +0300

    testing v1.1.0rc1

--- py_ballisticcalc_exts/py_ballisticcalc_exts/__init__.py
+++ py_ballisticcalc_exts/py_ballisticcalc_exts/__init__.py
@@ -2,7 +2,7 @@ __author__ = "o-murphy"
 __copyright__ = ("",)
 
 __credits__ = ["o-murphy"]
-__version__ = "1.1.0b10"
+__version__ = "1.1.0rc1"
 
 from .drag_model import *
 from .trajectory_calc import *
