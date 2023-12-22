6802365775f6d5ea4929d12268f44196b2047352
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2023-12-22 17:14:36 +0200

    v1.1.2-until_distance-fix (#42)

--- py_ballisticcalc/trajectory_calc.py
+++ py_ballisticcalc/trajectory_calc.py
@@ -266,7 +266,7 @@ class TrajectoryCalc:
                 if current_wind == len_winds - 1:
                     next_wind_range = 1e7
                 else:
-                    next_wind_range = winds[current_wind].until_distance() >> Distance.Foot
+                    next_wind_range = winds[current_wind].until_distance >> Distance.Foot
 
             # Zero-crossing checks
             if range_vector.x > 0:
