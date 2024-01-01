ceccd50c6250b544681839f00a40b3d60812a951
David Bookstaber <99043839+dbookstaber@users.noreply.github.com>
2024-01-01 16:05:49 -0700

    Update __init__.py

--- py_ballisticcalc/__init__.py
+++ py_ballisticcalc/__init__.py
@@ -1,2 +1,3 @@
 __all__ = ['interface', 'atmosphere', 'bmath', 'drag', 'drag_tables', 'projectile', 'shot_parameters',
            'trajectory_calculator', 'trajectory_data', 'wind', 'multiple_bc']
+__version__ = "1.1.2"
