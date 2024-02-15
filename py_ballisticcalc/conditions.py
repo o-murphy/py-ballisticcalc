3e65537703d8548c076674c3da732833f0f99d88
dbookstaber <99043839+dbookstaber@users.noreply.github.com>
2024-02-15 15:34:44 -0700

    Refactored and revalidated Atmosphere.

--- py_ballisticcalc/conditions.py
+++ py_ballisticcalc/conditions.py
@@ -9,25 +9,29 @@ from .munition import Weapon, Ammo
 
 __all__ = ('Atmo', 'Wind', 'Shot')
 
-cDegreesFtoR: float = 459.67  # °R = °F + 459.67
-cDegreesCtoK: float = 273.15  # °K = °C + 273.15
-cLapseRateImperial: float = -3.56616e-03  # Lapse rate, °F/ft
-cPressureExponent: float = -5.255876      # =g*M/R*L
-cSpeedOfSoundImperial: float = 49.0223    # Mach1 in fps = cSpeedOfSound * sqrt(°R)
-cSpeedOfSoundMetric: float = 331.3        # Mach1 in m/s = cSpeedOfSound * sqrt(°K)
-cDensityImperialToMetric: float = 16.0185 # lb/ft^3 to kg/m^3
+cStandardHumidity: float = 0.0            # Relative Humidity
+cPressureExponent: float = 5.255876     # =g*M/R*L
 cA0: float = 1.24871
 cA1: float = 0.0988438
 cA2: float = 0.00152907
 cA3: float = -3.07031e-06
 cA4: float = 4.21329e-07
 cA5: float = 3.342e-04
+# ISA, metric units: (https://www.engineeringtoolbox.com/international-standard-atmosphere-d_985.html)
+cDegreesCtoK: float = 273.15              # °K = °C + 273.15
+cStandardTemperatureC: float = 15.0       # °C
+cLapseRateMetric: float = -6.5e-03        # Lapse Rate, °C/m
+cStandardPressureMetric: float = 1013.25  # hPa
+cSpeedOfSoundMetric: float = 331.3        # Mach1 in m/s = cSpeedOfSound * sqrt(°K)
+cStandardDensityMetric: float = 1.2250    # kg/m^3
+cDensityImperialToMetric: float = 16.0185 # lb/ft^3 to kg/m^3
 # ICAO standard atmosphere:
-cStandardTemperatureR: float = 518.67  # °R=59°F
-cStandardTemperatureF: float = 59.0    # °F
-cStandardPressure: float = 29.92       # InHg
-cStandardDensity: float = 0.076474     # lb/ft^3
-cStandardHumidity: float = 0.0         # Relative Humidity
+cDegreesFtoR: float = 459.67              # °R = °F + 459.67
+cStandardTemperatureF: float = 59.0       # °F
+cLapseRateImperial: float = -3.56616e-03  # Lapse rate, °F/ft
+cStandardPressure: float = 29.92          # InHg
+cSpeedOfSoundImperial: float = 49.0223    # Mach1 in fps = cSpeedOfSound * sqrt(°R)
+cStandardDensity: float = 0.076474        # lb/ft^3
 
 @dataclass
 class Atmo(TypedUnits):  # pylint: disable=too-many-instance-attributes
@@ -35,14 +39,14 @@ class Atmo(TypedUnits):  # pylint: disable=too-many-instance-attributes
     altitude: [float, Distance] = field(default_factory=lambda: Set.Units.distance)
     pressure: [float, Pressure] = field(default_factory=lambda: Set.Units.pressure)
     temperature: [float, Temperature] = field(default_factory=lambda: Set.Units.temperature)
-    humidity: float = 0
-    density: float = field(init=False)
-    mach: Velocity = field(init=False)
-    _mach1: Velocity = field(init=False)
-    _a0: float = field(init=False)
-    _t0: float = field(init=False)
-    _p0: float = field(init=False)
-    _ta: float = field(init=False)
+    humidity: float = 0.0           # Relative humidity [0% to 100%]
+    density: float = field(init=False)  # Imperial (lb/ft^3)
+    mach: Velocity = field(init=False)  # Mach 1 in reference atmosphere
+    _mach1: float = field(init=False)  # Mach 1 in reference atmosphere in fps
+    _a0: float = field(init=False)  # Initial reference altitude (ft)
+    _t0: float = field(init=False)  # Temperature given at reference altitude °F
+    _p0: float = field(init=False)  # Barometric pressure (sea level)
+    _ta: float = field(init=False)  # Standard temperature at reference altitude °F
 
     def __post_init__(self):
         if self.humidity > 1:
@@ -54,9 +58,15 @@ class Atmo(TypedUnits):  # pylint: disable=too-many-instance-attributes
         if not self.temperature:
             self.temperature = Atmo.standard_temperature(self.altitude)
         if not self.pressure:
-            self.pressure = Atmo.standard_pressure(self.altitude)#, self.temperature)
+            self.pressure =  Atmo.standard_pressure(self.altitude)
 
-        self.calculate()
+        self._t0 = self.temperature >> Temperature.Fahrenheit
+        self._p0 = self.pressure >> Pressure.InHg
+        self._a0 = self.altitude >> Distance.Foot
+        self._ta = self._a0 * cLapseRateImperial + cStandardTemperatureF
+        self.density = self.calculate_density(self._t0, self._p0)
+        self._mach1 = Atmo.machF(self._t0)
+        self.mach = Velocity.FPS(self._mach1)
 
     @staticmethod
     def standard_temperature(altitude: Distance) -> Temperature:
@@ -69,11 +79,19 @@ class Atmo(TypedUnits):  # pylint: disable=too-many-instance-attributes
         """ICAO standard pressure for altitude"""
         return Pressure.InHg(0.02953
             * math.pow(3.73145 - (2.56555e-05) * (altitude >> Distance.Foot),
-                       -cPressureExponent)
+                       cPressureExponent)
             )
+        """Metric formula
+        Pressure.hPa(cStandardPressureMetric
+            * math.pow(1 - cLapseRateMetric * (altitude >> Distance.Meter) / (cStandardTemperatureC + cDegreesCtoK),
+                       cPressureExponent))
+        """
 
     @staticmethod
     def standard(altitude: [float, Distance] = 0, temperature: Temperature=None):
+        """Creates standard ICAO atmosphere at given altitude.
+            If temperature not specified uses standard temperature.
+        """
         return Atmo.icao(altitude, temperature)
 
     @staticmethod
@@ -84,14 +102,7 @@ class Atmo(TypedUnits):  # pylint: disable=too-many-instance-attributes
         altitude = Set.Units.distance(altitude)
         if temperature is None:
             temperature = Atmo.standard_temperature(altitude)
-
-        # TODO: Pretty sure this needs to be a function of altitude too?
-        pressure = Pressure.InHg(
-            cStandardPressure * math.pow(cStandardTemperatureR
-                / ((temperature >> Temperature.Fahrenheit) + cDegreesFtoR),
-                                         cPressureExponent
-                                        )
-        )
+        pressure = Atmo.standard_pressure(altitude)
 
         return Atmo(
             altitude >> Set.Units.distance,
@@ -110,18 +121,38 @@ class Atmo(TypedUnits):  # pylint: disable=too-many-instance-attributes
         """:return: Mach 1 in m/s for Celsius temperature"""
         return math.sqrt(1+celsius / cDegreesCtoK) * cSpeedOfSoundMetric
 
+    @staticmethod
+    def air_density(t: Temperature, p: Pressure, humidity: float) -> float:
+        """Source: https://en.wikipedia.org/wiki/Density_of_air#Humid_air
+        :return: Density in Imperial units (lb/ft^3)
+        """
+        tC = t >> Temperature.Celsius
+        pM = (p >> Pressure.hPa) * 100  # Pressure in Pascals
+        # Tetens approximation to saturation vapor pressure:
+        psat = 6.1078*math.pow(10, 17.27 * tC / (tC + 237.3))  
+        pv = humidity*psat  # Pressure of water vapor in Pascals
+        pd = pM - pv    # Partial pressure of dry air in Pascals
+        # Density in metric units kg/m^3
+        density = (pd*0.0289652 + pv*0.018016)/(8.31446 *(tC + cDegreesCtoK))
+        return density / cDensityImperialToMetric
+
     def temperature_at_altitude(self, altitude: float) -> float:
         """ Interpolated temperature at altitude
+        :param altitude: ASL in ft
         :return: temperature in °F
         """
         return (altitude - self._a0) * cLapseRateImperial + self._t0
 
     def density_factor(self) -> float:
-        """:return: projectile density_factor"""
+        """:return: air density ratio"""
         return self.density / cStandardDensity
 
-    def calculate0(self, t, p) -> (float, float):
-        """:return: density and mach with specified atmosphere"""
+    def calculate_density(self, t: float, p: float) -> float:
+        """DEPRECATED
+        :param t: temperature in °F
+        :param p: pressure in inHg
+        :return: density with specified atmosphere
+        """
         if t > 0:
             et0 = cA0 + t * (cA1 + t * (cA2 + t * (cA3 + t * cA4)))
             et = cA5 * self.humidity * et0
@@ -130,34 +161,25 @@ class Atmo(TypedUnits):  # pylint: disable=too-many-instance-attributes
             hc = 1.0
 
         density = cStandardDensity * (
-                cStandardTemperatureR / (t + cDegreesFtoR)
+                (cStandardTemperatureF + cDegreesFtoR) / (t + cDegreesFtoR)
         ) * hc
-        mach = math.sqrt(t + cDegreesFtoR) * cSpeedOfSoundImperial
-        return density, mach
-
-    def calculate(self) -> None:
-        """prepare the data for the calculation"""
-        self._t0 = self.temperature >> Temperature.Fahrenheit
-        self._p0 = self.pressure >> Pressure.InHg
-        self._a0 = self.altitude >> Distance.Foot
-        self._ta = self._a0 * cLapseRateImperial + cStandardTemperatureF
-
-        self.density, self._mach1 = self.calculate0(self._t0, self._p0)
-        self.mach = Velocity(self._mach1, Velocity.FPS)
+        return density
 
     def get_density_factor_and_mach_for_altitude(self, altitude: float):
-        """:return: density factor for the specified altitude"""
+        """
+        :param altitude: ASL in units of feet
+        :return: density ratio and Mach 1 (fps) for the specified altitude
+        """
+        # Within 30 ft of initial altitude use initial values
         if math.fabs(self._a0 - altitude) < 30:
-            density = self.density / cStandardDensity
+            density_ratio = self.density / cStandardDensity
             mach = self._mach1
-            return density, mach
-
-        tb = altitude * cLapseRateImperial + cStandardTemperatureF
-        t = self._t0 + self._ta - tb
-        p = self._p0 * math.pow(self._t0 / t, cPressureExponent)
-
-        density, mach = self.calculate0(t, p)
-        return density / cStandardDensity, mach
+        else:
+            t = self.temperature_at_altitude(altitude)
+            # https://en.wikipedia.org/wiki/Density_of_air#Exponential_approximation
+            density_ratio = math.exp(-altitude/34112.0)
+            mach = Atmo.machF(t)
+        return density_ratio, mach
 
 
 @dataclass
