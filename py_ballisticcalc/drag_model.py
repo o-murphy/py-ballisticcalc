f2a2cb3c46e36044deb5a3e81764a2d1ffcac999
dbookstaber <99043839+dbookstaber@users.noreply.github.com>
2024-04-03 13:31:14 -0700

    Finished MultiBC DragModel implementation.

--- py_ballisticcalc/drag_model.py
+++ py_ballisticcalc/drag_model.py
@@ -8,7 +8,7 @@ from .settings import Settings as Set
 from .unit import Weight, Distance, Velocity
 #from .drag_tables import DragTablesSet
 
-__all__ = ('DragModel', 'DragDataPoint', 'BCpoint')
+__all__ = ('DragModel', 'DragDataPoint', 'BCpoint', 'DragModelMultiBC')
 
 cSpeedOfSoundMetric = 340.0  # Speed of sound in standard atmosphere, in m/s
 
@@ -38,8 +38,6 @@ class DragModel:
     :param BC: Ballistic Coefficient of bullet = weight / diameter^2 / i,
             where weight is in pounds, diameter is in inches, and
             i is the bullet's form factor relative to the selected drag model.
-        Or List[BCpoint], and BC will be interpolated and applied to the .drag_table
-            (in which case self.BC = 1)
     :param drag_table: If passed as List of {Mach, CD} dictionaries, this
             will be converted to a List of DragDataPoints.
     :param weight: Bullet weight in grains
@@ -47,7 +45,7 @@ class DragModel:
     :param length: Bullet length in inches
     NOTE: .weight, .diameter, .length are only relevant for computing spin drift
     """
-    def __init__(self, BC: [float, list[BCpoint]],
+    def __init__(self, BC: float,
                  drag_table: typing.Iterable,
                  weight: [float, Weight]=0,
                  diameter: [float, Distance]=0,
@@ -55,7 +53,7 @@ class DragModel:
         error = ''
         if len(drag_table) <= 0:
             error = 'Received empty drag table'
-        elif isinstance(BC, float) and (BC <= 0):
+        elif BC <= 0:
             error = 'Ballistic coefficient must be positive'
         if error:
             raise ValueError(error)
@@ -63,18 +61,7 @@ class DragModel:
         self.drag_table = drag_table if isinstance(drag_table[0], DragDataPoint)\
             else make_data_points(drag_table) # Convert from list of dicts to list of DragDataPoints
 
-        # BC is a list, so generate new drag table by interpolating the BCpoints
-        if isinstance(BC, list):
-            BC = sorted(BC)  # Make sure we're sorted for np.interp
-            self.BCinterp = linear_interpolation([x['Mach'] for x in drag_table],
-                                         [x.Mach for x in BC],
-                                         [x.BC for x in BC])
-            for i in range(len(self.drag_table)):
-                self.drag_table[i].CD = self.drag_table[i].CD / self.BCinterp[i]
-            self.BC = 1.0
-        else:
-            self.BC = BC
-
+        self.BC = BC
         self.length = Set.Units.length(length)
         self.weight = Set.Units.weight(weight)
         self.diameter = Set.Units.diameter(diameter)
@@ -100,8 +87,43 @@ def make_data_points(drag_table: typing.Iterable) -> list[BCpoint]:
 
 
 def sectional_density(weight: float, diameter: float) -> float:
-    """Sectional density in lbs/in^2"""
+    """
+    :param weight: Projectile weight in grains
+    :param diameter: Projectile diameter in inches
+    :return: Sectional density in lbs/in^2
+    """
     return weight / math.pow(diameter, 2) / 7000
+
+
+def DragModelMultiBC(BCpoints: list[BCpoint], drag_table: typing.Iterable,
+                 weight: [float, Weight]=0,
+                 diameter: [float, Distance]=0,
+                 length: [float, Distance]=0) -> DragModel:
+    """Compute a drag model based on multiple BCs.
+        If weight and diameter are provided then we set BC=sectional density.
+        Otherwise we set BC=1 and the drag_table contains final drag terms.
+    :param BC:
+    :param weight: Bullet weight in grains
+    :param diameter: Bullet diameter in inches
+    :param length: Bullet length in inches
+    """
+    weight = Set.Units.weight(weight)
+    diameter = Set.Units.diameter(diameter)
+    if weight > 0 and diameter > 0:
+        BC = sectional_density(weight >> Weight.Grain, diameter >> Distance.Inch)
+    else:
+        BC = 1.0
+
+    drag_table = drag_table if isinstance(drag_table[0], DragDataPoint)\
+        else make_data_points(drag_table) # Convert from list of dicts to list of DragDataPoints
+
+    BCpoints = sorted(BCpoints)  # Make sure BCpoints are sorted for linear interpolation
+    BCinterp = linear_interpolation([x.Mach for x in drag_table],
+                                    [x.Mach for x in BCpoints],
+                                    [x.BC / BC for x in BCpoints])
+    for i in range(len(drag_table)):
+        drag_table[i].CD = drag_table[i].CD / BCinterp[i]
+    return DragModel(BC, drag_table, weight, diameter, length)
 
 
 def linear_interpolation(x: list[float], xp: list[float], yp: list[float]) -> list[float]:
--- tests/test_mbc.py
+++ tests/test_mbc.py
@@ -19,7 +19,7 @@ class TestMBC(unittest.TestCase):
 
     def test_mbc1(self):
         "We should get the same trajectory whether we give single BC or use multi-BC with single value"
-        dm_multi = DragModel([BCpoint(.22, V=Velocity.FPS(2500)), BCpoint(.22, V=Velocity.FPS(1500)), BCpoint(BC=.22, Mach=3)], TableG7)
+        dm_multi = DragModelMultiBC([BCpoint(.22, V=Velocity.FPS(2500)), BCpoint(.22, V=Velocity.FPS(1500)), BCpoint(BC=.22, Mach=3)], TableG7)
         multi_shot = Shot(weapon=self.weapon, ammo=Ammo(dm_multi, self.ammo.mv))
         multi_trajectory = self.calc.fire(shot=multi_shot, trajectory_range=self.range, trajectory_step=self.step).trajectory
         for i in range(len(multi_trajectory)):
@@ -27,7 +27,7 @@ class TestMBC(unittest.TestCase):
 
     def test_mbc2(self):
         "Setting different BC above muzzle velocity should have no effect"
-        dm_multi = DragModel([BCpoint(.22, V=Velocity.FPS(2700)), BCpoint(.5, V=Velocity.FPS(3500))], TableG7)
+        dm_multi = DragModelMultiBC([BCpoint(.22, V=Velocity.FPS(2700)), BCpoint(.5, V=Velocity.FPS(3500))], TableG7)
         multi_shot = Shot(weapon=self.weapon, ammo=Ammo(dm_multi, self.ammo.mv))
         multi_trajectory = self.calc.fire(shot=multi_shot, trajectory_range=self.range, trajectory_step=self.step).trajectory
         for i in range(len(multi_trajectory)):
@@ -36,10 +36,37 @@ class TestMBC(unittest.TestCase):
     def test_mbc3(self):
         "Setting higher BC should result in higher downrange velocities"
         # So here we'll boost the BC for velocities lower than the baseline's velocity at 200 yards
-        dm_multi = DragModel([BCpoint(.5, V=self.baseline_trajectory[3].velocity), BCpoint(.22, V=self.baseline_trajectory[2].velocity)], TableG7)
+        dm_multi = DragModelMultiBC([BCpoint(.5, V=self.baseline_trajectory[3].velocity), BCpoint(.22, V=self.baseline_trajectory[2].velocity)], TableG7)
         multi_shot = Shot(weapon=self.weapon, ammo=Ammo(dm_multi, self.ammo.mv))
         multi_trajectory = self.calc.fire(shot=multi_shot, trajectory_range=self.range, trajectory_step=self.step).trajectory
         # Should show no change before 200 yards
         self.assertEqual(multi_trajectory[1].velocity.raw_value, self.baseline_trajectory[1].velocity.raw_value)
         # Should be faster at any point after 200 yards
         self.assertGreater(multi_trajectory[4].velocity.raw_value, self.baseline_trajectory[4].velocity.raw_value)
+
+    def test_mbc(self):
+        dm = DragModelMultiBC([BCpoint(0.275, V=Velocity.MPS(800)), BCpoint(0.255, V=Velocity.MPS(500)), BCpoint(0.26, V=Velocity.MPS(700))],
+                               TableG7, weight=178, diameter=.308)
+        self.assertAlmostEqual(dm.drag_table[0].CD, 0.1259323091692403)
+        self.assertAlmostEqual(dm.drag_table[-1].CD, 0.1577125859466895)
+    
+    def test_mbc_valid(self):
+        # Litz's multi-bc table comversion to CDM, 338LM 285GR HORNADY ELD-M
+        dm = DragModelMultiBC([BCpoint(0.417, V=Velocity.MPS(745)), BCpoint(0.409, V=Velocity.MPS(662)), BCpoint(0.4, V=Velocity.MPS(580))],
+            drag_table=TableG7,
+            weight=Weight.Grain(285),
+            diameter=Distance.Inch(0.338)
+        )
+        cds = [p.CD for p in dm.drag_table]
+        machs = [p.Mach for p in dm.drag_table]
+        reference = (
+            (1, 0.3384895315),
+            (2, 0.2585639866),
+            (3, 0.2069547831),
+            (4, 0.1652052415),
+            (5, 0.1381406102),
+        )
+        for mach, cd in reference:
+            idx = machs.index(mach)
+            with self.subTest(mach=mach):
+                self.assertAlmostEqual(cds[idx], cd, 3)
