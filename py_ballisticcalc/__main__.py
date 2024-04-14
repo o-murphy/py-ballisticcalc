c54a9164559fd6ad23c17d431858a6a873d52ad2
o-murphy <thehelixpg@gmail.com>
2024-04-15 01:34:39 +0300

    separated argparser from __main__.main

--- py_ballisticcalc/__main__.py
+++ py_ballisticcalc/__main__.py
@@ -10,42 +10,57 @@ from importlib import metadata
 version = metadata.metadata("py_ballisticcalc")['Version']
 
 
-def main(argv):
+def add_general_arguments(parser):
+    general = parser.add_argument_group('General')
+    general.add_argument("-zd", "--shot-dist", action="store_true", help="Zero distance")
+    general.add_argument("-sd", "--zero-dist", action="store_true", help="Shot distance")
+
+
+def add_zero_atmo_group(parser):
+    zero_atmo = parser.add_argument_group('Zero atmo', 'Zero atmosphere parameters')
+    zero_atmo.add_argument("-zt", "--zero-t", action="store", help="Zero temperature")
+    zero_atmo.add_argument("-zh", "--zero-h", action="store", help="Zero humidity")
+    zero_atmo.add_argument("-zp", "--zero-p", action="store", help="Zero pressure")
+    zero_atmo.add_argument("-za", "--zero-a", action="store", help="Zero altitude")
+
+
+def add_shot_atmo_group(parser):
+    shot_atmo = parser.add_argument_group('Atmosphere', 'Current atmosphere parameters')
+    shot_atmo.add_argument("-at", "--atmo-t", action="store", help="Current temperature")
+    shot_atmo.add_argument("-ah", "--atmo-h", action="store", help="Current humidity")
+    shot_atmo.add_argument("-ap", "--atmo-p", action="store", help="Current pressure")
+    shot_atmo.add_argument("-aa", "--atmo-a", action="store", help="Current altitude")
+
+
+def add_wind_group(parser):
+    wind = parser.add_argument_group('Wind', 'Shot wind data')
+    wind.add_argument("-wv", "--wind-v", action="store", help="Wind velocity")
+    wind.add_argument("-wd", "--wind-d", action="store", help="Wind direction")
+
+
+def get_arg_parser():
+    parser = argparse.ArgumentParser(
+        prog=f'pybc v{version}',
+        description="Tool for small arms ballistics calculations"
+    )
+    parser.add_argument('files', help="List of .toml files to configure calculator",
+                        nargs='+', type=str)
+    parser.add_argument("-v", "--version", action='version',
+                        version=f'pybc v{version}', help="Show version")
+    parser.add_argument("-d", "--debug", action="store_true", help="Enable debug messages")
+
+    add_general_arguments(parser)
+    add_zero_atmo_group(parser)
+    add_shot_atmo_group(parser)
+    add_wind_group(parser)
+    return parser
+
+
+def main():
     try:
-        parser = argparse.ArgumentParser(
-            prog=f'pybc v{version}',
-            description="Tool for small arms ballistics calculations"
-        )
-        parser.add_argument('files', help="List of .toml files to configure calculator",
-                            nargs='+', type=str)
-        parser.add_argument("-v", "--version", action='version',
-                            version=f'pybc v{version}', help="Show version")
-        parser.add_argument("-d", "--debug", action="store_true", help="Enable debug messages")
-
-        general = parser.add_argument_group('General')
-
-        # TODO: also add to toml parser
-        general.add_argument("-zd", "--shot-dist", action="store_true", help="Zero distance")
-        general.add_argument("-sd", "--zero-dist", action="store_true", help="Shot distance")
-
-        zero_atmo = parser.add_argument_group('Zero atmo', 'Zero atmosphere parameters')
-        zero_atmo.add_argument("-zt", "--zero-t", action="store", help="Zero temperature")
-        zero_atmo.add_argument("-zh", "--zero-h", action="store", help="Zero humidity")
-        zero_atmo.add_argument("-zp", "--zero-p", action="store", help="Zero pressure")
-        zero_atmo.add_argument("-za", "--zero-a", action="store", help="Zero altitude")
-
-        shot_atmo = parser.add_argument_group('Atmosphere', 'Current atmosphere parameters')
-        shot_atmo.add_argument("-at", "--atmo-t", action="store", help="Current temperature")
-        shot_atmo.add_argument("-ah", "--atmo-h", action="store", help="Current humidity")
-        shot_atmo.add_argument("-ap", "--atmo-p", action="store", help="Current pressure")
-        shot_atmo.add_argument("-aa", "--atmo-a", action="store", help="Current altitude")
-
-        wind = parser.add_argument_group('Wind', 'Shot wind data')
-        wind.add_argument("-wv", "--wind-v", action="store", help="Wind velocity")
-        wind.add_argument("-wd", "--wind-d", action="store", help="Wind direction")
-
+        parser = get_arg_parser()
         argv = parser.parse_args()
-        print(argv)
+
         if argv.debug:
             logger.setLevel(logging.DEBUG)
             logger.info("Debug messages enabled")
@@ -62,4 +77,4 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
