7b0bc5f7c18f8a89b627229bb1973a485d392c75
o-murphy <thehelixpg@gmail.com>
2025-01-12 19:26:07 +0200

    pybc assets load methods

--- examples/config_load.py
+++ examples/config_load.py
@@ -1,14 +1,24 @@
-from py_ballisticcalc import (basicConfig, PreferredUnits,
+from py_ballisticcalc import (basicConfig, PreferredUnits, loadMixedUnits,
                               get_global_use_powder_sensitivity, get_global_max_calc_step_size)
 
-basicConfig("../assets/.pybc-imperial.toml")
+import importlib.resources
 
+# with importlib.resources.files('py_ballisticcalc').joinpath('.pybc.toml') as config_file:
+#     basicConfig(config_file)
+
+with importlib.resources.files('py_ballisticcalc').joinpath('assets/.pybc-imperial.toml') as config_file:
+    basicConfig(config_file)
+
+print("Imperial:")
 print(PreferredUnits)
 print(get_global_use_powder_sensitivity())
 print(get_global_max_calc_step_size())
 
-basicConfig("../assets/.pybc-mixed.toml")
+print()
 
+loadMixedUnits()
+
+print("Mixed:")
 print(PreferredUnits)
 print(get_global_use_powder_sensitivity())
 print(get_global_max_calc_step_size())
--- py_ballisticcalc/__init__.py
+++ py_ballisticcalc/__init__.py
@@ -10,6 +10,8 @@ __credits__ = ["o-murphy", "dbookstaber"]
 
 import os
 import sys
+import importlib.resources
+
 from typing_extensions import Dict, Union, Optional
 
 from .trajectory_calc import *
@@ -113,14 +115,38 @@ def _basic_config(filename=None,
         _load_config(filename)
 
 
+def _resolve_resource_path(path: os.PathLike):
+    with importlib.resources.files('py_ballisticcalc').joinpath(path) as config_file:
+        return config_file
+
+
+def _load_imperial_units():
+    _basic_config(_resolve_resource_path('assets/.pybc-imperial.toml'))
+
+
+def _load_metric_units():
+    _basic_config(_resolve_resource_path('assets/.pybc-metrics.toml'))
+
+
+def _load_mixed_units():
+    _basic_config(_resolve_resource_path('assets/.pybc-mixed.toml'))
+
+loadImperialUnits = _load_imperial_units
+loadMetricUnits = _load_metric_units
+loadMixedUnits = _load_mixed_units
+
 basicConfig = _basic_config
 
 basicConfig()
+
 
 # pylint: disable=duplicate-code
 __all__ = [
     'Calculator',
     'basicConfig',
+    'loadImperialUnits',
+    'loadMetricUnits',
+    'loadMixedUnits',
     'logger',
     'TrajectoryCalc',
     'Vector',
@@ -183,12 +209,13 @@ __all__ += [
     'TableGS'
 ]
 
-
 try:
     # check if cython based extensions installed
     import py_ballisticcalc_exts  # type: ignore
+
     logger.debug("Binary modules found, running in binary mode")
 except ImportError as error:
     import warnings
+
     warnings.warn("Library running in pure python mode. "
                   "For better performance install 'py_ballisticcalc.exts' binary package", UserWarning)
