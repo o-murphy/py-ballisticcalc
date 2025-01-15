4be8a712e9057a8bc802782b49da472a39dec756
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2025-01-15 17:45:11 +0200

    * import fix for RKBallistic support

--- examples/performance_check.py
+++ examples/performance_check.py
@@ -52,26 +52,26 @@ number = 120
 zero_distance = Unit.Meter(100)
 shot_distance = Unit.Meter(1000)
 
-# total_time = timeit(lambda: rk4.barrel_elevation_for_target(shot, zero_distance), number=number)
-# rate = number / total_time  # executions per second
-#
-# print("Calculate barrel elevation at distance {} {} times:".format(zero_distance, number))
-# print(f"Total time: {total_time:.6f} seconds")
-# print(f"Execution rate: {rate:.2f} calls per second")
-#
-# total_time = timeit(lambda: rk4.fire(shot, shot_distance, extra_data=False), number=number)
-# rate = number / total_time  # executions per second
-#
-# print("Calculate trajectory to distance {} {} times:".format(shot_distance, number))
-# print(f"Total time: {total_time:.6f} seconds")
-# print(f"Execution rate: {rate:.2f} calls per second")
-#
-# total_time = timeit(lambda: rk4.fire(shot, shot_distance, extra_data=True), number=number)
-# rate = number / total_time  # executions per second
-#
-# print("Calculate trajectory to distance {} {} times:".format(shot_distance, number))
-# print(f"Total time: {total_time:.6f} seconds")
-# print(f"Execution rate: {rate:.2f} calls per second")
+total_time = timeit(lambda: rk4.barrel_elevation_for_target(shot, zero_distance), number=number)
+rate = number / total_time  # executions per second
+
+print("Calculate barrel elevation at distance {} {} times:".format(zero_distance, number))
+print(f"Total time: {total_time:.6f} seconds")
+print(f"Execution rate: {rate:.2f} calls per second")
+
+total_time = timeit(lambda: rk4.fire(shot, shot_distance, extra_data=False), number=number)
+rate = number / total_time  # executions per second
+
+print("Calculate trajectory to distance {} {} times:".format(shot_distance, number))
+print(f"Total time: {total_time:.6f} seconds")
+print(f"Execution rate: {rate:.2f} calls per second")
+
+total_time = timeit(lambda: rk4.fire(shot, shot_distance, extra_data=True), number=number)
+rate = number / total_time  # executions per second
+
+print("Calculate trajectory to distance {} {} times:".format(shot_distance, number))
+print(f"Total time: {total_time:.6f} seconds")
+print(f"Execution rate: {rate:.2f} calls per second")
 print()
 
 total_time = timeit(lambda: calc.barrel_elevation_for_target(shot, zero_distance), number=number)
