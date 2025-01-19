db2c4bebeb6cc3584781d39ad13b597cdace65e5
o-murphy <thehelixpg@gmail.com>
2025-01-19 03:32:07 +0200

    cibuildwheel fix

--- py_ballisticcalc/trajectory_data/__init__.py
+++ py_ballisticcalc/trajectory_data/__init__.py
@@ -1,14 +1,10 @@
 from py_ballisticcalc.trajectory_data._trajectory_data import HitResult, TrajFlag, DangerSpace
 from py_ballisticcalc.logger import logger
-import platform
 
 
 try:
-    if platform.python_implementation() == "PyPy":
-        from py_ballisticcalc.trajectory_data._trajectory_data import TrajectoryData
-    else:
-        # replace with cython based implementation
-        from py_ballisticcalc_exts import TrajectoryData  # type: ignore
+    # replace with cython based implementation
+    from py_ballisticcalc_exts import TrajectoryData  # type: ignore
 except ImportError as err:
     from py_ballisticcalc.trajectory_data._trajectory_data import TrajectoryData
     logger.debug(err)
