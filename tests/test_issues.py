d07db9400221316ec4ec5a1669344014a492328a
Serhiy Yevtushenko <syevtushenko@gmail.com>
2025-01-22 10:12:11 +0100

    Fix for issue https://github.com/o-murphy/py-ballisticcalc/issues/141 with tests.

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -1,15 +1,18 @@
 """Unittests for the specific issues library"""
-
+import copy
+# mypy: ignore - mypy overhead is not worth it for test code
 import unittest
+from typing import Any
 
 from typing_extensions import Union
 from py_ballisticcalc import (DragModel, TableG1, Distance, Weight, Ammo, Velocity, Weapon, Shot,
-                              Angular, Calculator, RangeError, HitResult)
+                              Angular, Calculator, RangeError, HitResult, InterfaceConfigDict, loadImperialUnits,
+                              loadMetricUnits, PreferredUnits)
 
 
 class TestIssue96_97(unittest.TestCase):
 
-    def setUp(self) -> None:
+    def setUp(self):
         drag_model = DragModel(bc=0.03,
                                drag_table=TableG1,
                                diameter=Distance.Millimeter(23),
@@ -18,7 +21,7 @@ class TestIssue96_97(unittest.TestCase):
         ammo = Ammo(drag_model, Velocity.MPS(930))
         weapon = Weapon()
         self.zero = Shot(weapon=weapon, ammo=ammo, relative_angle=Angular.Degree(1.0))
-        self.calc = Calculator(_config={"cMinimumVelocity": 0})
+        self.calc = Calculator(_config=InterfaceConfigDict(cMinimumVelocity=0))
         self.trange = Distance.Meter(1600.2437248702522)
 
     def test_must_return_hit_result(self):
@@ -37,9 +40,118 @@ class TestIssue96_97(unittest.TestCase):
                 # directly init hit result with incomplete data before exception occurred
                 return HitResult(zero_shot, err.incomplete_trajectory, extra=extra_data), err
 
+
         hit_result, err = must_fire(self.calc, self.zero, self.trange, extra_data=True)
 
         # should return error
         self.assertIsInstance(err, RangeError)
         self.assertIsInstance(hit_result, HitResult, f"Expected HitResult but got {type(hit_result)}")
+        
+def get_object_attribute_values_as_dict(obj: Any)->dict[str, Any]:
+    """"""
+    return {attr: getattr(obj, attr) for attr in dir(obj) if not attr.startswith("__")}
 
+
+class TestIssue144(unittest.TestCase):
+
+    def setUp(self):
+        # storing preferred unit settings in order to avoid influencing other tests
+        # due to preferred unit being singletons
+        self.previous_preferred_units = get_object_attribute_values_as_dict(PreferredUnits)
+        drag_model = DragModel(
+            bc=0.759,
+            drag_table=TableG1,
+            weight=Weight.Gram(108),
+            diameter=Distance.Millimeter(23),
+            length=Distance.Millimeter(108.2),
+        )
+        weapon = Weapon()
+        ammo = Ammo(drag_model, Velocity.MPS(930))
+        self.shot = Shot(weapon=weapon, ammo=ammo, relative_angle=Angular.Degree(13.122126582196692))
+        self.range = Distance.Meter(740.8068308628336)
+        self.calc = Calculator()
+
+    
+    def tearDown(self):
+        PreferredUnits.set(**self.previous_preferred_units)
+
+    def testResultsWithImperialUnits(self):
+        loadImperialUnits()
+        hit_result = self.calc.fire(self.shot, self.range, extra_data=False)
+        self.assertEqual(len(hit_result.trajectory), 11)
+        last_hit_point = hit_result[-1]
+        self.assertEqual(last_hit_point.time, 0.9920863205706615)
+        self.assertEqual((last_hit_point.distance>>Distance.Meter), 740.8498567639497)
+        self.assertEqual((last_hit_point.height >> Distance.Meter), 168.4355597904274)
+
+    def testResultsWithImperialUnits_FloatInput(self):
+        loadImperialUnits()
+        hit_result = self.calc.fire(self.shot, self.range>>PreferredUnits.distance, extra_data=False)
+        self.assertEqual(len(hit_result.trajectory), 11)
+        last_hit_point = hit_result[-1]
+        self.assertEqual(last_hit_point.time, 0.9920863205706615)
+        self.assertEqual((last_hit_point.distance>>Distance.Meter), 740.8498567639497)
+        self.assertEqual((last_hit_point.height >> Distance.Meter), 168.4355597904274)
+
+
+    def testResultsWithMetricUnits(self):
+        loadMetricUnits()
+        hit_result = self.calc.fire(self.shot, self.range, extra_data=False)
+        self.assertEqual(len(hit_result.trajectory), 11)
+        last_hit_point = hit_result[-1]
+        self.assertEqual(last_hit_point.time, 0.9920863205706615)
+        self.assertEqual((last_hit_point.distance>>Distance.Meter), 740.8498567639497)
+        self.assertEqual((last_hit_point.height >> Distance.Meter), 168.4355597904274)
+
+    def testResultsWithMetricUnits_FloatInput(self):
+        loadMetricUnits()
+        hit_result = self.calc.fire(self.shot, self.range>>PreferredUnits.distance, extra_data=False)
+        self.assertEqual(len(hit_result.trajectory), 11)
+        last_hit_point = hit_result[-1]
+        self.assertEqual(last_hit_point.time, 0.9920863205706615)
+        self.assertEqual((last_hit_point.distance>>Distance.Meter), 740.8498567639497)
+        self.assertEqual((last_hit_point.height >> Distance.Meter), 168.4355597904274)
+
+
+    def testResultsWithMetricUnits_FloatTrajectoryStep(self):
+        loadMetricUnits()
+        hit_result = self.calc.fire(self.shot, self.range, trajectory_step=Distance.Inch(2916.5623262316285)>>PreferredUnits.distance,
+                                    extra_data=False)
+        self.assertEqual(len(hit_result.trajectory), 11)
+        last_hit_point = hit_result[-1]
+        self.assertEqual(last_hit_point.time, 0.9920863205706615)
+        self.assertEqual((last_hit_point.distance>>Distance.Meter), 740.8498567639497)
+        self.assertEqual((last_hit_point.height >> Distance.Meter), 168.4355597904274)
+
+
+    def testResultsWithImperialUnitsAndYards(self):
+        loadImperialUnits()
+        PreferredUnits.distance = Distance.Yard
+        hit_result = self.calc.fire(self.shot, self.range, extra_data=False)
+        self.assertEqual(len(hit_result.trajectory), 11)
+        last_hit_point = hit_result[-1]
+        self.assertEqual(last_hit_point.time, 0.9920863205706615)
+        self.assertEqual((last_hit_point.distance>>Distance.Meter), 740.8498567639497)
+        self.assertEqual((last_hit_point.height >> Distance.Meter), 168.4355597904274)
+
+    def testResultsWithImperialUnitAndYards_UnitTrajectoryStep(self):
+        loadImperialUnits()
+        PreferredUnits.distance = Distance.Yard
+        hit_result = self.calc.fire(self.shot, self.range, trajectory_step=Distance.Inch(2916.5623262316285),
+                                    extra_data=False)
+        self.assertEqual(len(hit_result.trajectory), 11)
+        last_hit_point = hit_result[-1]
+        self.assertEqual(last_hit_point.time, 0.9920863205706615)
+        self.assertEqual((last_hit_point.distance>>Distance.Meter), 740.8498567639497)
+        self.assertEqual((last_hit_point.height >> Distance.Meter), 168.4355597904274)
+
+
+    def testResultWithImperialUnits_FloatRange(self):
+        loadImperialUnits()
+        self.assertEqual(PreferredUnits.distance, Distance.Foot)
+        hit_result = self.calc.fire(self.shot, self.range>>Distance.Foot, extra_data=False)
+        self.assertEqual(len(hit_result.trajectory), 11)
+        last_hit_point = hit_result[-1]
+        self.assertEqual(last_hit_point.time, 0.9920863205706615)
+        self.assertEqual((last_hit_point.distance>>Distance.Meter), 740.8498567639497)
+        self.assertEqual((last_hit_point.height >> Distance.Meter), 168.4355597904274)
