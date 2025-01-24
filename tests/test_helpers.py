b488719953f00d51f54e6a90db5a3e59a1300a4f
Serhiy Yevtushenko <syevtushenko@gmail.com>
2025-01-24 17:12:04 +0100

    Changed precision in one test. Commented out part of test_find_index_for_timepoint in order to perform differential debugging

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -59,65 +59,65 @@ def test_find_index_for_timepoint(one_degree_shot):
     assert abs(one_degree_shot.trajectory[index].time - one_second_time_point) <= abs(
         one_degree_shot.trajectory[index + 1].time - one_second_time_point
     )
-
-    # when strictly_bigger_or_equal is Treu, we are finding first existing time point, which is biiger or equal to
-    # the specified time
-    bigger_index = find_index_for_time_point(
-        one_degree_shot, one_second_time_point, strictly_bigger_or_equal=True
-    )
-    assert bigger_index == index + 1
-
-    shot_max_time_point = one_degree_shot[-1].time
-
-    # Edge case testing - we should be able to find the last available time point in both modes
-    last_point_index = find_index_for_time_point(
-        one_degree_shot, shot_max_time_point, strictly_bigger_or_equal=False
-    )
-    assert last_point_index != -1
-    # print(f"{shot_max_time_point=} {one_degree_shot.trajectory[last_point_index].time=} {last_point_index=}" )
-    # print(f"{shot_max_time_point=} {one_degree_shot.trajectory[len(one_degree_shot.trajectory) - 1].time=} {len(one_degree_shot.trajectory) - 1=}")
-    assert last_point_index == len(one_degree_shot.trajectory) - 1
-
-    last_point_index = find_index_for_time_point(
-        one_degree_shot, shot_max_time_point, strictly_bigger_or_equal=True
-    )
-    assert last_point_index != -1
-    assert last_point_index == len(one_degree_shot.trajectory) - 1
-
-    # if deviation of searched time point is equal to max_time_deviation_in_seconds, then last point should be found
-    index = find_index_for_time_point(
-        one_degree_shot,
-        shot_max_time_point + 1,
-        strictly_bigger_or_equal=False,
-        max_time_deviation_in_seconds=1,
-    )
-    assert index == len(one_degree_shot.trajectory) - 1
-
-    index = find_index_for_time_point(
-        one_degree_shot,
-        shot_max_time_point + 1,
-        strictly_bigger_or_equal=False,
-        max_time_deviation_in_seconds=0.5,
-    )
-    assert index == -1
-
-    # we should be able to find first timepoint in both modes
-    index = find_index_for_time_point(
-        one_degree_shot, 0, strictly_bigger_or_equal=False
-    )
-    assert 0 == index
-
-    index = find_index_for_time_point(one_degree_shot, 0, strictly_bigger_or_equal=True)
-    assert 0 == index
-
-    # exception should be thrown on negative time
-    with pytest.raises(ValueError):
-        find_index_for_time_point(one_degree_shot, -1)
-
-    # exception should be thrown on negative max_time_deviation
-    with pytest.raises(ValueError):
-        find_index_for_time_point(one_degree_shot, 0, max_time_deviation_in_seconds=-1)
-
+    #
+    # # when strictly_bigger_or_equal is Treu, we are finding first existing time point, which is biiger or equal to
+    # # the specified time
+    # bigger_index = find_index_for_time_point(
+    #     one_degree_shot, one_second_time_point, strictly_bigger_or_equal=True
+    # )
+    # assert bigger_index == index + 1
+    #
+    # shot_max_time_point = one_degree_shot[-1].time
+    #
+    # # Edge case testing - we should be able to find the last available time point in both modes
+    # last_point_index = find_index_for_time_point(
+    #     one_degree_shot, shot_max_time_point, strictly_bigger_or_equal=False
+    # )
+    # assert last_point_index != -1
+    # # print(f"{shot_max_time_point=} {one_degree_shot.trajectory[last_point_index].time=} {last_point_index=}" )
+    # # print(f"{shot_max_time_point=} {one_degree_shot.trajectory[len(one_degree_shot.trajectory) - 1].time=} {len(one_degree_shot.trajectory) - 1=}")
+    # assert last_point_index == len(one_degree_shot.trajectory) - 1
+    #
+    # last_point_index = find_index_for_time_point(
+    #     one_degree_shot, shot_max_time_point, strictly_bigger_or_equal=True
+    # )
+    # assert last_point_index != -1
+    # assert last_point_index == len(one_degree_shot.trajectory) - 1
+    #
+    # # if deviation of searched time point is equal to max_time_deviation_in_seconds, then last point should be found
+    # index = find_index_for_time_point(
+    #     one_degree_shot,
+    #     shot_max_time_point + 1,
+    #     strictly_bigger_or_equal=False,
+    #     max_time_deviation_in_seconds=1,
+    # )
+    # assert index == len(one_degree_shot.trajectory) - 1
+    #
+    # index = find_index_for_time_point(
+    #     one_degree_shot,
+    #     shot_max_time_point + 1,
+    #     strictly_bigger_or_equal=False,
+    #     max_time_deviation_in_seconds=0.5,
+    # )
+    # assert index == -1
+    #
+    # # we should be able to find first timepoint in both modes
+    # index = find_index_for_time_point(
+    #     one_degree_shot, 0, strictly_bigger_or_equal=False
+    # )
+    # assert 0 == index
+    #
+    # index = find_index_for_time_point(one_degree_shot, 0, strictly_bigger_or_equal=True)
+    # assert 0 == index
+    #
+    # # exception should be thrown on negative time
+    # with pytest.raises(ValueError):
+    #     find_index_for_time_point(one_degree_shot, -1)
+    #
+    # # exception should be thrown on negative max_time_deviation
+    # with pytest.raises(ValueError):
+    #     find_index_for_time_point(one_degree_shot, 0, max_time_deviation_in_seconds=-1)
+    #
 
 def test_find_index_for_distance(one_degree_shot):
     shot = one_degree_shot
@@ -154,7 +154,7 @@ def test_find_apex(one_degree_shot):
     index = find_index_of_apex_point(one_degree_shot)
     assert index!=-1
     apex_point_height = one_degree_shot.trajectory[index].height >> Distance.Meter
-    assert apex_point_height==9.401238429883138
+    assert apex_point_height==pytest.approx(9.40, abs=0.01)
 
 
 
