27e0bba418cb7bfce96da40328605d45513efdbf
o-murphy <thehelixpg@gmail.com>
2025-03-25 21:19:09 +0200

    typing fix

--- py_ballisticcalc/vector/_vector.py
+++ py_ballisticcalc/vector/_vector.py
@@ -81,7 +81,7 @@ class Vector(NamedTuple):
             return Vector(self.x, self.y, self.z)
         return self.mul_by_const(1.0 / m)
 
-    def __mul__(self, other: Union[int, float, 'Vector']) -> Union[float, 'Vector']:
+    def __mul__(self, other: Union[int, float, 'Vector']) -> Union[float, 'Vector']:  # type: ignore[override]
         if isinstance(other, (int, float)):
             return self.mul_by_const(other)
         if isinstance(other, Vector):
@@ -89,12 +89,29 @@ class Vector(NamedTuple):
         raise TypeError(other)
 
     # aliases more efficient than wrappers
-    __add__ = add
-    __radd__ = add
-    __iadd__ = add
-    __sub__ = subtract
-    __rsub__ = subtract
-    __isub__ = subtract
-    __rmul__ = __mul__
-    __imul__ = __mul__
-    __neg__ = negate
+    def __add__(self, other: 'Vector') -> 'Vector':  # type: ignore[override]
+        return self.add(other)
+
+    def __radd__(self, other: 'Vector') -> 'Vector':  # type: ignore[override]
+        return self.add(other)
+
+    def __iadd__(self, other: 'Vector') -> 'Vector':  # type: ignore[override]
+        return self.add(other)
+
+    def __sub__(self, other: 'Vector') -> 'Vector':  # type: ignore[override]
+        return self.subtract(other)
+
+    def __rsub__(self, other: 'Vector') -> 'Vector':  # type: ignore[override]
+        return self.subtract(other)
+
+    def __isub__(self, other: 'Vector') -> 'Vector':  # type: ignore[override]
+        return self.subtract(other)
+
+    def __rmul__(self, other: Union[int, float, 'Vector']) -> Union[float, 'Vector']:  # type: ignore[override]
+        return self.__mul__(other)
+
+    def __imul__(self, other: Union[int, float, 'Vector']) -> Union[float, 'Vector']:  # type: ignore[override]
+        return self.__mul__(other)
+
+    def __neg__(self) -> 'Vector':  # type: ignore[override]
+        return self.negate()
